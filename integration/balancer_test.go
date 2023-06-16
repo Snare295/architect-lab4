e4a306fdf34805009ece0c4b78ea958001155763
romalitv <youlikeyoupass@gmail.com>
2023-06-16 04:41:19 +0200

    Seeing work of integration tests in workflow

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -38,11 +38,11 @@ func (s *MySuite) TestBalancer(c *C) {
 
 	// Testing headers of servers
 	server1Header := server1.Header.Get("lb-from")
-	c.Assert(server1Header, Equals, "server1:8080", Commentf("Wrong server for server1 - %s", server1Header))
+	c.Assert(server1Header, Equals, "server2:8080", Commentf("Wrong server for server1 - %s", server1Header))
 	server2Header := server2.Header.Get("lb-from")
-	c.Assert(server2Header, Equals, "server2:8080", Commentf("Wrong server for server2 - %s", server2Header))
+	c.Assert(server2Header, Equals, "server3:8080", Commentf("Wrong server for server2 - %s", server2Header))
 	server3Header := server3.Header.Get("lb-from")
-	c.Assert(server3Header, Equals, "server3:8080", Commentf("Wrong server for server3 - %s", server3Header))
+	c.Assert(server3Header, Equals, "server1:8080", Commentf("Wrong server for server3 - %s", server3Header))
 	server1RepeatHeader := server1Repeat.Header.Get("lb-from")
 	c.Assert(server1RepeatHeader, Equals, server1Header, Commentf("Headers are not equal. origin - %s, repeat - %s", server1Header, server1RepeatHeader))
 }
